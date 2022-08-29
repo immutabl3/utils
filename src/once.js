34b4de154333f1631380b9cf7cb8bc869f3ca6b8
Joseph Clay <joe@weareimmutable.com>
2022-08-29 16:53:16 -0500

    pass args

--- src/once.js
+++ src/once.js
@@ -1,9 +1,9 @@
 export default function once(fn) {
   let memo;
   let called = false;
-  return function once() {
+  return function once(...args) {
     if (called) return memo;
     called = true;
-    return (memo = fn());
+    return (memo = fn(...args));
   };
 };
